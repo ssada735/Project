efcdb64c74f520bcb34795d39f657ce4b72d4398
ssada735 <79553201+ssada735@users.noreply.github.com>
2021-11-29 19:37:58 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+img = "";
+status = "";
+function preload()
+{
+img = loadImage('dog_cat.jpg');
+}
+function setup() {
+canvas = createCanvas(640,420);
+canvas.center();
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function modelLoaded() {
+console.log("Model Loaded!");
+status = true;
+objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results) {
+if (error) {
+console.log(error);
+}
+console.log(results);
+}
+
+function draw() {
+image(img, 0,0, 640, 420);
+fill("#FF0000");
+text("Bed Room", 45, 75);
+noFill();
+stroke("#FF0000");
+rect(30, 60, 450, 350);
+}
+fill("#FF0000");
+text("TV/AC", 45, 75);
+noFill();
+stroke("#FF0000");
+rect(30, 60, 450, 350);
+
+fill("#FF0000");
+text("Desks", 45, 75);
+noFill();
+stroke("#FF0000");
+rect(30, 60, 450, 350);
+
+fill("#FF0000");
+text("Bottles", 45, 75);
+noFill();
+stroke("#FF0000");
+rect(30, 60, 450, 350);
+
+fill("#FF0000");
+text("Fruit Basket", 45, 75);
+noFill();
+stroke("#FF0000");
+rect(30, 60, 450, 350);
